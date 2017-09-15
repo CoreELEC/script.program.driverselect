10bc1cc3eb07b969e873c0b7f132a6b1756b6746
Birger Jesch <birger.jesch@gmail.com>
2017-09-15 18:47:51 +0200

    WIP 0.0.2

--- default.py
+++ default.py
@@ -10,8 +10,8 @@ if response is not None:
 
     addon_list = {}
     gui_list = []
+    modules = []
     item = 0
-    default_old = None
 
     # get all services, discard services without driver.dvb signature
 
@@ -22,7 +22,7 @@ if response is not None:
                                 'enabled': service.get('enabled', False)}})
         if service.get('enabled', False):
             gui_list.append('[B]' + service.get('name', '') + '[/B]')
-            default_old = service.get('addonid')
+            modules.append(service.get('addonid'))
         else:
             gui_list.append(service.get('name', ''))
         item += 1
@@ -35,39 +35,40 @@ if response is not None:
 
         if driver_module > -1:
 
-            # disable old driver module
+            # disable old driver module(s)
 
-            query = {"method": "Addons.SetAddonEnabled",
-                     "params":{"addonid": default_old, "enabled": False}}
-            response = jsonrpc(query)
-            if response == 'OK':
-                writeLog('driver module \'%s\' disabled' % (default_old))
-
-                # enable new driver module
-
+            for dvbmodule in modules:
                 query = {"method": "Addons.SetAddonEnabled",
-                         "params": {"addonid": addon_list[driver_module]['addonid'], "enabled": True}}
+                         "params":{"addonid": dvbmodule, "enabled": False}}
                 response = jsonrpc(query)
                 if response == 'OK':
-                    writeLog('driver module \'%s\' enabled' % (addon_list[driver_module]['addonid']))
-
-                    # ask for reboot
-
-                    yesno = dialogYesNo(LS(30012), LS(30013))
-                    if yesno:
-                        query = {"method": "System.Reboot",
-                                 "params":{}}
-                        response = jsonrpc(query)
-                        if response == 'OK':
-                            writeLog('system will now reboot')
-                        else:
-                            writeLog('could not reboot', xbmc.LOGFATAL)
-                    else:
-                        notify(LS(30010), LS(30014))
+                    writeLog('driver module \'%s\' disabled' % (dvbmodule))
                 else:
-                    writeLog('could not enable driver module \'%s\'' % (addon_list[driver_module]['addonid']), xbmc.LOGFATAL)
+                    writeLog('could not disable driver module \'%s\'' % (dvbmodule), xbmc.LOGFATAL)
+
+            # enable new driver module
+
+            query = {"method": "Addons.SetAddonEnabled",
+                     "params": {"addonid": addon_list[driver_module]['addonid'], "enabled": True}}
+            response = jsonrpc(query)
+            if response == 'OK':
+                writeLog('driver module \'%s\' enabled' % (addon_list[driver_module]['addonid']))
+
+                # ask for reboot
+
+                yesno = dialogYesNo(LS(30012), LS(30013))
+                if yesno:
+                    query = {"method": "System.Reboot",
+                             "params":{}}
+                    response = jsonrpc(query)
+                    if response == 'OK':
+                        writeLog('system will now reboot')
+                    else:
+                        writeLog('could not reboot', xbmc.LOGFATAL)
+                else:
+                    notify(LS(30010), LS(30014))
             else:
-                writeLog('could not disable driver module \'%s\'' % (default_old), xbmc.LOGFATAL)
+                writeLog('could not enable driver module \'%s\'' % (addon_list[driver_module]['addonid']), xbmc.LOGFATAL)
     else:
         writeLog('no driver modules found', xbmc.LOGFATAL)
         notify(LS(30010), LS(30015))

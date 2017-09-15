d6564a4c9c77941304cb856e8925346635ea9634
Birger Jesch <birger.jesch@gmail.com>
2017-09-15 18:19:54 +0200

    Initial commit

---
+++ default.py
@@ -0,0 +1,73 @@
+from resources.lib.tools import *
+
+writeLog('starting addon')
+query = {"method": "Addons.GetAddons",
+         "params": {"type": "xbmc.service",
+                    "properties": ["name", "path", "enabled"]}}
+response = jsonrpc(query)
+if response is not None:
+    writeLog('get services')
+
+    addon_list = {}
+    gui_list = []
+    item = 0
+    default_old = None
+
+    # get all services, discard services without driver.dvb signature
+
+    for service in response['addons']:
+        if not 'driver.dvb.' in service.get('addonid', ''): continue
+        addon_list.update({item: {'name': service.get('name', ''),
+                                'addonid': service.get('addonid', ''),
+                                'enabled': service.get('enabled', False)}})
+        if service.get('enabled', False):
+            gui_list.append('[B]' + service.get('name', '') + '[/B]')
+            default_old = service.get('addonid')
+        else:
+            gui_list.append(service.get('name', ''))
+        item += 1
+
+    # show selections
+
+    if item > 0:
+        driver_module = dialogSelect(LS(30011), gui_list)
+        writeLog('selected item: %s' % (driver_module))
+
+        if driver_module > -1:
+
+            # disable old driver module
+
+            query = {"method": "Addons.SetAddonEnabled",
+                     "params":{"addonid": default_old, "enabled": False}}
+            response = jsonrpc(query)
+            if response == 'OK':
+                writeLog('driver module \'%s\' disabled' % (default_old))
+
+                # enable new driver module
+
+                query = {"method": "Addons.SetAddonEnabled",
+                         "params": {"addonid": addon_list[driver_module]['addonid'], "enabled": True}}
+                response = jsonrpc(query)
+                if response == 'OK':
+                    writeLog('driver module \'%s\' enabled' % (addon_list[driver_module]['addonid']))
+
+                    # ask for reboot
+
+                    yesno = dialogYesNo(LS(30012), LS(30013))
+                    if yesno:
+                        query = {"method": "System.Reboot",
+                                 "params":{}}
+                        response = jsonrpc(query)
+                        if response == 'OK':
+                            writeLog('system will now reboot')
+                        else:
+                            writeLog('could not reboot', xbmc.LOGFATAL)
+                    else:
+                        notify(LS(30010), LS(30014))
+                else:
+                    writeLog('could not enable driver module \'%s\'' % (addon_list[driver_module]['addonid']), xbmc.LOGFATAL)
+            else:
+                writeLog('could not disable driver module \'%s\'' % (default_old), xbmc.LOGFATAL)
+    else:
+        writeLog('no driver modules found', xbmc.LOGFATAL)
+        notify(LS(30010), LS(30015))
---
+++ resources/lib/tools.py
@@ -0,0 +1,78 @@
+import xbmc
+import xbmcgui
+import xbmcaddon
+import json
+import platform
+import re
+
+ADDON_NAME = xbmcaddon.Addon().getAddonInfo('name')
+PATH = xbmcaddon.Addon().getAddonInfo('path')
+PROFILE = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile'))
+LS = xbmcaddon.Addon().getLocalizedString
+
+# Constants
+
+STRING = 0
+BOOL = 1
+NUM = 2
+
+
+def writeLog(message, level=xbmc.LOGDEBUG):
+    xbmc.log('[%s %s] %s' % (xbmcaddon.Addon().getAddonInfo('id'),
+                             xbmcaddon.Addon().getAddonInfo('version'),
+                             message.encode('utf-8')), level)
+
+
+def notify(header, message, icon=xbmcgui.NOTIFICATION_INFO, dispTime=5000):
+    xbmcgui.Dialog().notification(header.encode('utf-8'), message.encode('utf-8'), icon=icon, time=dispTime)
+
+
+def release():
+    item = {}
+    coll = {'platform': platform.system(), 'hostname': platform.node()}
+    if coll['platform'] == 'Linux':
+        with open('/etc/os-release', 'r') as _file:
+            for _line in _file:
+                parameter, value = _line.split('=')
+                item[parameter] = value.replace('"', '').strip()
+
+    coll.update({'osname': item.get('NAME', ''), 'osid': item.get('ID', ''), 'osversion': item.get('VERSION_ID', '')})
+    return coll
+
+
+def dialogOK(header, message):
+    return xbmcgui.Dialog().ok(header.encode('utf-8'), message.encode('utf-8'))
+
+
+def dialogYesNo(header, message):
+    return xbmcgui.Dialog().yesno(header.encode('utf-8'), message.encode('utf-8'))
+
+
+def dialogSelect(header, itemlist):
+    _itemlist = []
+    for item in itemlist:
+        _itemlist.append(item.encode('utf-8'))
+    return xbmcgui.Dialog().select(header.encode('utf-8'), _itemlist)
+
+
+def jsonrpc(query):
+    querystring = {"jsonrpc": "2.0", "id": 1}
+    querystring.update(query)
+    try:
+        response = json.loads(xbmc.executeJSONRPC(json.dumps(querystring, encoding='utf-8')))
+        if 'result' in response: return response['result']
+    except TypeError, e:
+        writeLog('Error executing JSON RPC: %s' % (e.message), xbmc.LOGFATAL)
+    return None
+
+
+def getAddonSetting(setting, sType=STRING, multiplicator=1):
+    if sType == BOOL:
+        return  True if xbmcaddon.Addon().getSetting(setting).upper() == 'TRUE' else False
+    elif sType == NUM:
+        try:
+            return int(re.match('\d+', xbmcaddon.Addon().getSetting(setting)).group()) * multiplicator
+        except AttributeError:
+            return 0
+    else:
+        return xbmcaddon.Addon().getSetting(setting)
